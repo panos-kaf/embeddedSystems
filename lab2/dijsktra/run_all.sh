7f71da235e285641bb27c0999b5ab44cef2dc16d
Panos Kaf <panoskatsalifis@gmail.com>
2025-11-23 19:47:01 +0200

    modify script

--- lab2/dijsktra/run_all.sh
+++ lab2/dijsktra/run_all.sh
@@ -4,15 +4,23 @@ qList="SLL DLL DYN_ARR"
 
 out_dir="results"
 
-mkdir -p $out_dir
+massif_dir="$out_dir/massif_out"
+footprints_dir="$out_dir/mem_footprint"
+accesses_dir="$out_dir/mem_accesses"
+logs_dir="$out_dir/mem_logs"
+
+mkdir -p $massif_dir
+mkdir -p $footprints_dir
+mkdir -p $accesses_dir
+mkdir -p $logs_dir
 
 for list in $qList
 do
     make $list
-    log="${out_dir}/mem_accesses_log_${list}.txt"
+    log="${logs_dir}/${list}.txt"
     valgrind --log-file="${log}" --tool=lackey --trace-mem=yes ./dijkstra input.dat
-    grep -c 'I\| L' ${log} > ${out_dir}/numMemAccesses_${list}.txt
-    massif_out="${out_dir}/massif.out.${list}"
+    grep -c 'I\| L' ${log} > ${acceses_dir}/${list}.txt
+    massif_out="${massif_dir}/${list}"
     valgrind --tool=massif --massif-out-file=${massif_out} ./dijkstra input.dat
-    ms_print ${massif_out} > mem_footprint_log_${list}.txt
+    ms_print ${massif_out} > ${footprints_dir}/${list}.txt
 done
