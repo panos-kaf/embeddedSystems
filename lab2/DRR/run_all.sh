96fffda07db75515c59e528876e367f596f8c521
Panos Kaf <panoskatsalifis@gmail.com>
2025-11-23 20:35:33 +0200

    update scripts

--- lab2/DRR/run_all.sh
+++ lab2/DRR/run_all.sh
@@ -6,21 +6,24 @@ packetList="SLL_PK DLL_PK DYN_ARR_PK"
 
 out_dir="results"
 
-mkdir -p ${outdir}/mem_logs
-mkdir -p ${outdir}/numAccesses
-mkdir -p ${outdir}/mem_footprint
-mkdir -p ${outdir}/massif_out
+logs_dir="${out_dir}/mem_logs"
+accesses_dir="${out_dir}/numAccesses"
+footprints_dir="${out_dir}/mem_footprint"
+massif_dir="${out_dir}/massif_out"
+
+mkdir -p "$logs_dir" "$accesses_dir" "$footprints_dir" "$massif_dir"
 
 for cl in $clientList
 do
     for pl in $packetList
     do
+        name="${cl}_${pl}.txt"
         gcc drr.c -o drr -D${cl} -D${pl} -pthread -lcdsl -no-pie -L ./../synch_implementations -I ./../synch_implementations
-        log="${out_dir}/mem_logs/mem_accesses_log_${cl}_${pl}.txt"
+        log="${logs_dir}/${name}"
         valgrind --log-file="${log}" --tool=lackey --trace-mem=yes ./drr
-        grep -c 'I\| L' ${log} > ${out_dir}/numAccesses/numMemAccesses_${cl}_${pl}.txt
-        massif_out="${out_dir}/massif_out/massif.out.${cl}_${pl}"
+        grep -c 'I\| L' ${log} > ${accesses_dir}/${name}
+        massif_out="${massif_dir}/${name}"
         valgrind --tool=massif --massif-out-file=${massif_out} ./drr
-        ms_print ${massif_out} > ${out_dir}/mem_footprint/mem_footprint_log_${cl}_${pl}.txt
+        ms_print ${massif_out} > ${footprints_dir}/${name}
     done
 done
--- lab2/dijsktra/run_all.sh
+++ lab2/dijsktra/run_all.sh
@@ -9,18 +9,16 @@ footprints_dir="$out_dir/mem_footprint"
 accesses_dir="$out_dir/mem_accesses"
 logs_dir="$out_dir/mem_logs"
 
-mkdir -p $massif_dir
-mkdir -p $footprints_dir
-mkdir -p $accesses_dir
-mkdir -p $logs_dir
+mkdir -p "$logs_dir" "$accesses_dir" "$footprints_dir" "$massif_dir"
 
 for list in $qList
 do
     make $list
-    log="${logs_dir}/${list}.txt"
+    name="${list}.txt"
+    log="${logs_dir}/${name}"
     valgrind --log-file="${log}" --tool=lackey --trace-mem=yes ./dijkstra input.dat
-    grep -c 'I\| L' ${log} > ${acceses_dir}/${list}.txt
-    massif_out="${massif_dir}/${list}"
+    grep -c 'I\| L' ${log} > ${accesses_dir}/${name}
+    massif_out="${massif_dir}/${name}"
     valgrind --tool=massif --massif-out-file=${massif_out} ./dijkstra input.dat
-    ms_print ${massif_out} > ${footprints_dir}/${list}.txt
+    ms_print ${massif_out} > ${footprints_dir}/${name}
 done
